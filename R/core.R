6c9e419f759b0cb2f1b9dcacf5a88b300f721290
Sadatsafavi <mohsen.sadatsafavi@ubc.ca>
2021-07-09 10:20:49 -0700

    GRupload and GRrun added

--- R/core.R
+++ R/core.R
@@ -118,11 +118,12 @@ GRpush<-function(Robject, overWrite=FALSE, fileName=NULL)
   {
     fileName <-paste0(GRcreateName(),".RDS")
   }
-  
+
   dir<-tempdir()
   local_file<-paste0(dir,"/",fileName)
   
   saveRDS(Robject,file = local_file)
+  
   rdrop2::drop_upload(file=local_file, path=GRglobalSettings$projectName)
   
   if(overWrite)
@@ -138,6 +139,28 @@ GRpush<-function(Robject, overWrite=FALSE, fileName=NULL)
   return(0)
 }
 
+
+
+
+#'@export 
+GRupload <- function(local_file, dest_file=NULL)
+{
+  GRcheckStatus()
+
+  tmp <- strsplit(local_file,"/")[[1]]
+  pure_file_name <- tmp[length(tmp)]
+  
+  tmp <- tempdir()
+  
+  if(!is.null(dest_file))
+  {
+    file.copy(from=local_file, to=paste0(tmp,"/",dest_file))
+    local_file <- paste0(tmp,"/",dest_file)
+  }
+  
+  try(rdrop2::drop_delete(path = paste0(GRglobalSettings$projectName,"/",pure_file_name)), silent = T)
+  rdrop2::drop_upload(file=local_file, path=paste0(GRglobalSettings$projectName))
+}
 
 
 
@@ -190,28 +213,13 @@ GRclean<-function()
 
 
 #'@export 
-GRrun<-function()
+GRrun<-function(file_name="run.R")
 {
+  local_folder <- tempdir()
   GRcheckStatus()
-  x<-GRlist()
-  if(is.na(match("/RUN",x))) stop("No RUN folder was found.")
-  path<-tempdir()
-  message(paste("Local folder is",path))
-  files<-GRlist("RUN")
-  for(fl in files)
-  {
-    if(substring(fl,1,1)=="/")
-    {
-      message("Warning: subfolder detectd; content ignored.")
-    }
-    else
-    {
-      message(paste("Doanloading",fl))
-      googledrive::drive_download(fl,paste0(path,"/",fl),overwrite = T)
-    }
-  }
-  setwd(path)
-  source(paste0(path,"/GRrun.R"))
+  rdrop2::drop_download(paste0(GRglobalSettings$projectName,"/",file_name),local_path =paste0(local_folder,"/",file_name),overwrite = T)
+  setwd(local_folder)
+  source(paste0(local_folder,"/",file_name))
 }
 
 
@@ -245,91 +253,6 @@ GRcollect<-function()
 
 
 
-
-#'@export
-GRformatOutput<-function(x,rGroupVars,cGroupVars,func=GRformatOutput.defaultProcessor)
-{
-  x<-as.data.frame(x)
-  
-  allGroups<-paste0(paste0(rGroupVars,collapse=","),",",paste0(cGroupVars,collapse=","))
-  ssql<-paste0("SELECT ", allGroups," FROM x GROUP BY ",allGroups)
-  y<-sqldf(ssql)
-  
-  z<-sqldf(paste0("SELECT DISTINCT ", paste0(rGroupVars,collapse=",") ," FROM y"))
-  n_row<-dim(z)[1]
-  row_names<-rep("",n_row)
-  for(i in 1:dim(z)[2])
-  {
-    if(i>1) row_names<-paste0(row_names,",")
-    row_names<-paste0(row_names,paste0(names(z)[i],":",z[,i]))
-  }
-  z<-sqldf(paste0("SELECT DISTINCT ", paste0(cGroupVars,collapse=",") ," FROM y"))
-  n_col<-dim(z)[1]
-  col_names<-rep("",n_col)
-  for(i in 1:dim(z)[2])
-  {
-    if(i>1) col_names<-paste0(col_names,",")
-    col_names<-paste0(col_names,paste0(names(z)[i],":",z[,i]))
-  }
-  
-  
-  ssql<-"SELECT * FROM x WHERE 1"
-  for(i in 1:dim(y)[2])
-  {
-    ssql<-paste0(ssql," AND ",colnames(y)[i],"=")
-    if(is.numeric(y[,i])) 
-    {
-      ssql<-paste0(ssql,"%f")
-    }
-    else
-    {
-      ssql<-paste0(ssql,"'%s'")
-    }
-  }
-  
-  message(ssql)
-  
-  out<-structure(rep(" ",n_row*n_col),.Dim=c(n_row,n_col))
-  rownames(out)<-row_names
-  colnames(out)<-col_names
-  i_row<-1
-  i_col<-1
-  for(i in 1:(n_row*n_col))
-  {
-    cat(".")
-    ssql2<-do.call(sprintf,args=c(ssql,y[i,]))
-    
-    res<-func(sqldf(ssql2))
-    out[i_row,i_col]<-res
-    i_col<-i_col+1
-    if(i_col>n_col)
-    {
-      i_row<-i_row+1
-      i_col<-1
-    }
-  }
-  return(out)
-}
-
-
-
-
-
-
-GRformatOutput.defaultProcessor<-function(data)
-{
-  out<-""
-  n<-dim(data)[2]
-  nms<-colnames(data)
-  for(i in 1:n)
-  {
-    if(is.numeric(data[,i]))
-    {
-      out<-paste0(out,nms[i],":mean=",mean(data[,i]),"(SD=",sd(data[,i]),"); ")
-    }
-  }
-  return(out)
-}
 
 
 
